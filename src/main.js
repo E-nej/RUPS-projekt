fa5cd6046e3fc0f3d02ede1dca1509e3fdded0e5
Aleša <alesa.kolednik@gmail.com>
2025-10-20 15:31:14 +0200

    Nastavljena glavna datoteka main.js

--- src/main.js
+++ src/main.js
@@ -1,2 +1,30 @@
 import './style.css'
+import Phaser from 'phaser';
 
+// uvoz scen
+
+
+const config = {
+  type: Phaser.AUTO,            
+  width: 800,                    
+  height: 600,                   
+  backgroundColor: '#f4f6fa',    
+  parent: 'game-container',      
+  scene: [
+    // uvoz scen
+  ],
+  physics: {
+    default: 'arcade',           
+    arcade: {
+      gravity: { y: 0 },         
+      debug: false               
+    }
+  },
+  scale: {
+    mode: Phaser.Scale.FIT,      
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  }
+};
+
+
+new Phaser.Game(config);
