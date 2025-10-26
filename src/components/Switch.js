da150c17ff1d219e2b63a8536489075c28f2c809
Crabo1 <enej.krabonja@gmail.com>
2025-10-26 14:02:22 +0100

    Stikalo

---
+++ src/components/Switch.js
@@ -0,0 +1,67 @@
+import { DraggableComponent } from './DraggableComponent';
+
+export class Switch extends DraggableComponent {
+    constructor(x, y) {
+        super(x, y);
+        this.isOpen = true; // true = off, false = on
+        this.width = 60;
+        this.height = 40;
+        this.terminals = [
+            { x: 0, y: 0 },
+            { x: this.width, y: 0 }
+        ];
+    }
+
+    toggle() {
+        this.isOpen = !this.isOpen;
+    }
+
+    isPointInside(x, y) {
+        return x >= this.x - this.width / 2 && 
+               x <= this.x + this.width / 2 &&
+               y >= this.y - this.height / 2 && 
+               y <= this.y + this.height / 2;
+    }
+
+    getConnectors() {
+        return this.terminals.map(t => ({
+            x: this.x - this.width / 2 + t.x,
+            y: this.y + t.y,
+            component: this
+        }));
+    }
+
+    draw(ctx) {
+        ctx.save();
+        ctx.translate(this.x, this.y);
+
+        ctx.strokeStyle = '#000';
+        ctx.lineWidth = 2;
+
+        // terminal posts
+        ctx.fillStyle = '#888';
+        ctx.beginPath();
+        ctx.arc(-30, 0, 4, 0, Math.PI * 2);
+        ctx.arc(30, 0, 4, 0, Math.PI * 2);
+        ctx.fill();
+
+        // switch lever
+        ctx.strokeStyle = this.isOpen ? '#C00' : '#0C0';
+        ctx.lineWidth = 3;
+        ctx.beginPath();
+        ctx.moveTo(-25, 0);
+        if (this.isOpen) {
+            ctx.lineTo(15, -15); // open
+        } else {
+            ctx.lineTo(25, 0); // close
+        }
+        ctx.stroke();
+
+        // label
+        ctx.fillStyle = '#000';
+        ctx.font = '12px Arial';
+        ctx.fillText(this.isOpen ? 'OFF' : 'ON', -12, 25);
+
+        ctx.restore();
+    }
+}
